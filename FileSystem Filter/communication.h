f0162bfe91a185c96701e99298c782481354d632
hwangyoungjun <hyj765@naver.com>
2023-09-15 16:52:34 +0900

    filesystem filter study start

---
+++ FileSystem Filter/communication.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<fltKernel.h>
+#include<ntddk.h>
+
+
+namespace Communicate {
+
+	NTSTATUS StartCommunicate(PFLT_FILTER filter,
+		PFLT_PORT port,
+		PFLT_CONNECT_NOTIFY notifyCallback,
+		PFLT_DISCONNECT_NOTIFY disConnectCallback,
+		PFLT_MESSAGE_NOTIFY messageCallback,
+		LONG MaxConnection,
+		UNICODE_STRING name);
+
+	void CloseCommunication(PFLT_PORT port);
+
+	NTSTATUS DataSend(PFLT_FILTER filterHandle,PFLT_PORT clientPort, PVOID buffer, ULONG bufferSize, PVOID response, ULONG responseSize, ULONG timeOut);
+
+}
